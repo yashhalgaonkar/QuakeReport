f7a18a80ea873542d4586b754896444618034851
Yash <yash.halgaonkar18@vit.edu>
2019-11-12 16:31:03 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# QuakeReport
+Android app to give information about earthquakes around the world.
+The app uses API and https requests for collecting its data from online resources.
---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,25 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+    private String mMagnitude;
+    private String mLocation;
+    private String mDate;
+
+    public Earthquake(String mMagnitude, String mLocation, String mDate) {
+        this.mMagnitude = mMagnitude;
+        this.mLocation = mLocation;
+        this.mDate = mDate;
+    }
+
+    public String getmMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+}
